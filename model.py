b74cfcae524d86bc86fe3499107b9c132b5906a8
Crazgen <yohh.life@gmail.com>
2016-07-31 01:26:36 +0800

    Model test pass

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
 import pulp as lp
+Clerk_i_d = 'Clerks included'
 
 Clerk_m_s_h = 'Clerk minimum shifting hour'
 Clerk_m_s = 'Clerk multiple shift'
@@ -10,15 +11,16 @@ Clerk_m_w_d = 'Clerk most whole day'
 Clerk_m_r = 'minimum clerks'
 Clerk_m_t = 'Clerk minimum work time'
 Clerk_o_t = 'Clerk most over time'
-Clerk_i_d = 'Clerks included'
 Clerk_c_o = 'Clerk continuous off days'
 Clerk_w_b = 'Clerk weekly workday balance'
 Clerk_e_l_s = 'Clerk early late switch'
+CONSTRAINTS = [Clerk_m_s_h, Clerk_m_s, Clerk_w_t_b, Clerk_w_d_b, Clerk_m_o_d, Clerk_m_w_d, Clerk_m_r, Clerk_m_t,
+               Clerk_o_t, Clerk_c_o, Clerk_w_b, Clerk_e_l_s]
 
 
+# constraints in data: data['constraint name'] = (is_involved, must_meet, priority, optional_data_dicts)
 def solve_single_floor(data):
-    # constraints in data: data['constraint name'] = (is_involved, must_meet, priority, optional_data_dicts)
-    hour_num, day_num, clerk_num = data['working hours'], data['wording days'], data['clerks']
+    hour_num, day_num, clerk_num = data['working hours'], data['working days'], data['clerks']
     time_slots_num = hour_num * day_num
     clerks, time_slots, hours, days = (range(1, clerk_num+1), range(1, time_slots_num+1), range(1, hour_num+1),
                                        range(1, day_num+1))
@@ -26,11 +28,12 @@ def solve_single_floor(data):
     clerk_day = [(n, d) for n in clerks for d in days]
 
     prob = lp.LpProblem("Scheduling", lp.LpMinimize)
-    obj = lp.LpAffineExpression()
     # This part for clerk constraints
     x_c = lp.LpVariable.dicts('Time on duty', clerk_time, cat='Binary')
     z_c = lp.LpVariable.dicts('Day on duty', clerk_day, cat='Binary')
     kesi_c = lp.LpVariable.dicts('Switch on duty', clerk_time, cat='Binary')
+    cons_relax = {c_str: None for c_str in CONSTRAINTS}
+    obj = lp.LpAffineExpression()
     # first is the relations between different variables
     for n, d in clerk_day:
         prob += z_c[(n, d)] <= lp.lpSum(x_c[(n, hour_num*(d-1)+h)] for h in hours)
@@ -45,21 +48,22 @@ def solve_single_floor(data):
                 prob += (data[Clerk_m_s_h][3]['minimum hour'] * z_c[(n, d)] <=
                          lp.lpSum(x_c[(n, hour_num*(d-1)+h)] for h in hours))
         else:
-            cmsh = lp.LpVariable.dicts('Number of exceeding hours', clerk_day, 0)
-            obj += lp.lpSum(cmsh[n_d] for n_d in clerk_day) * (10**data[Clerk_m_s_h][2])
+            cons_relax[Clerk_m_s_h] = lp.LpVariable.dicts('Number of exceeding hours', clerk_day, 0)
+            obj += lp.lpSum(cons_relax[Clerk_m_s_h][n_d] for n_d in clerk_day) * (10 ** data[Clerk_m_s_h][2])
             for n, d in clerk_day:
                 prob += (data[Clerk_m_s_h][3]['minimum hour'] * z_c[(n, d)] <=
-                         lp.lpSum(x_c[(n, hour_num * (d - 1) + h)] for h in hours) + cmsh[(n, d)])
+                         lp.lpSum(x_c[(n, hour_num * (d - 1) + h)] for h in hours) + cons_relax[Clerk_m_s_h][(n, d)])
     # constraint for no cross shifty
     if data[Clerk_m_s][0]:
         if data[Clerk_m_s][1]:
             for n, d in clerk_day:
                 prob += lp.lpSum(kesi_c[(n, hour_num*(d-1)+h)] for h in hours) <= 1
         else:
-            cms = lp.LpVariable.dicts('Number of cross shifting', clerk_day, 0)
-            obj += lp.lpSum(cms[n_d] for n_d in clerk_day) * (10**data[Clerk_m_s][2])
+            cons_relax[Clerk_m_s] = lp.LpVariable.dicts('Number of cross shifting', clerk_day, 0)
+            obj += lp.lpSum(cons_relax[Clerk_m_s][n_d] for n_d in clerk_day) * (10 ** data[Clerk_m_s][2])
             for n, d in clerk_day:
-                prob += lp.lpSum(kesi_c[(n, hour_num * (d - 1) + h)] for h in hours) <= 1 + cms[(n, d)]
+                prob += (lp.lpSum(kesi_c[(n, hour_num * (d - 1) + h)] for h in hours) <=
+                         1 + cons_relax[Clerk_m_s][(n, d)])
     # constraint for work time balance
     if data[Clerk_w_t_b][0]:
         clerk_wtb = data[Clerk_w_t_b][3][Clerk_i_d]
@@ -69,11 +73,11 @@ def solve_single_floor(data):
             for i, j in clerk_inter_wtb:
                 prob += lp.lpSum(x_c[(i, t)] for t in time_slots) - lp.lpSum(x_c[(j, t)] for t in time_slots) <= dif_wtb
         else:
-            cwtb = lp.LpVariable.dicts('Number of exceeding work hours', clerk_inter_wtb, 0)
-            obj += lp.lpSum(cwtb[i_j] for i_j in clerk_inter_wtb) * (10**data[Clerk_w_t_b][2])
+            cons_relax[Clerk_w_t_b] = lp.LpVariable.dicts('Number of exceeding work hours', clerk_inter_wtb, 0)
+            obj += lp.lpSum(cons_relax[Clerk_w_t_b][i_j] for i_j in clerk_inter_wtb) * (10 ** data[Clerk_w_t_b][2])
             for i, j in clerk_inter_wtb:
                 prob += (lp.lpSum(x_c[(i, t)] for t in time_slots) - lp.lpSum(x_c[(j, t)] for t in time_slots) <=
-                         dif_wtb + cwtb[(i, j)])
+                         dif_wtb + cons_relax[Clerk_w_t_b][(i, j)])
     # constraint for work day balance
     if data[Clerk_w_d_b][0]:
         clerk_wdb = data[Clerk_w_d_b][3][Clerk_i_d]
@@ -83,11 +87,11 @@ def solve_single_floor(data):
             for i, j in clerk_inter_wdb:
                 prob += lp.lpSum(z_c[(i, d)] for d in days) - lp.lpSum(z_c[(j, d)] for d in days) <= dif_wdb
         else:
-            cwdb = lp.LpVariable.dicts('Number of exceeding work days', clerk_inter_wdb, 0)
-            obj += lp.lpSum(cwdb[i_j] for i_j in clerk_inter_wdb) * (10**data[Clerk_w_d_b][2])
+            cons_relax[Clerk_w_d_b] = lp.LpVariable.dicts('Number of exceeding work days', clerk_inter_wdb, 0)
+            obj += lp.lpSum(cons_relax[Clerk_w_d_b][i_j] for i_j in clerk_inter_wdb) * (10 ** data[Clerk_w_d_b][2])
             for i, j in clerk_inter_wdb:
                 prob += (lp.lpSum(z_c[(i, d)] for d in days) - lp.lpSum(z_c[(j, d)] for d in days) <=
-                         dif_wdb + cwdb[(i, j)])
+                         dif_wdb + cons_relax[Clerk_w_d_b][(i, j)])
     # constraint for minimum off days in the period
     if data[Clerk_m_o_d][0]:
         clerk_mod = data[Clerk_m_o_d][3][Clerk_i_d]
@@ -96,10 +100,10 @@ def solve_single_floor(data):
             for n in clerk_mod:
                 prob += lp.lpSum(z_c[(n, d)] for d in days) <= day_num - mod
         else:
-            cmod = lp.LpVariable.dicts('Number of exceeding days', clerk_mod, 0)
-            obj += lp.lpSum(cmod[n] for n in clerk_mod) * (10**data[Clerk_m_o_d][2])
+            cons_relax[Clerk_m_o_d] = lp.LpVariable.dicts('Number of exceeding days', clerk_mod, 0)
+            obj += lp.lpSum(cons_relax[Clerk_m_o_d][n] for n in clerk_mod) * (10 ** data[Clerk_m_o_d][2])
             for n in clerk_mod:
-                prob += lp.lpSum(z_c[(n, d)] for d in days) <= day_num - mod + cmod[n]
+                prob += lp.lpSum(z_c[(n, d)] for d in days) <= day_num - mod + cons_relax[Clerk_m_o_d][n]
     # constraint for 最多连续上全班天数
     if data[Clerk_m_w_d][0]:
         clerk_mwd = data[Clerk_m_w_d][3][Clerk_i_d]
@@ -110,10 +114,11 @@ def solve_single_floor(data):
             for n, t in clerk_time_mwd:
                 prob += lp.lpSum(x_c[(n, t + k - 1)] for k in range(1, mwd+1)) <= mwd - 1
         else:
-            cmwd = lp.LpVariable.dicts('Number of exceeding whole days', clerk_time_mwd, 0)
-            obj += lp.lpSum(cmwd[n_t] for n_t in clerk_time_mwd) * (10**data[Clerk_m_w_d][2])
+            cons_relax[Clerk_m_w_d] = lp.LpVariable.dicts('Number of exceeding whole days', clerk_time_mwd, 0)
+            obj += lp.lpSum(cons_relax[Clerk_m_w_d][n_t] for n_t in clerk_time_mwd) * (10 ** data[Clerk_m_w_d][2])
             for n, t in clerk_time_mwd:
-                prob += lp.lpSum(x_c[(n, t + k - 1)] for k in range(1, mwd + 1)) <= mwd - 1 + cmwd[(n, t)]
+                prob += (lp.lpSum(x_c[(n, t + k - 1)] for k in range(1, mwd + 1)) <=
+                         mwd - 1 + cons_relax[Clerk_m_w_d][(n, t)])
     # constraint for minimum clerks required, combined with traffic..
     if data[Clerk_m_r][0]:
         mini_clerks = data[Clerk_m_r][3]['minimum clerk num']
@@ -121,10 +126,10 @@ def solve_single_floor(data):
             for t in time_slots:
                 prob += lp.lpSum(x_c[(n, t)] for n in clerks) >= mini_clerks[t]
         else:
-            cmr = lp.LpVariable.dicts('Number of short of clerks', time_slots, 0)
-            obj += lp.lpSum(cmr[t] for t in time_slots) * (10**data[Clerk_m_r][2])
+            cons_relax[Clerk_m_r] = lp.LpVariable.dicts('Number of short of clerks', time_slots, 0)
+            obj += lp.lpSum(cons_relax[Clerk_m_r][t] for t in time_slots) * (10 ** data[Clerk_m_r][2])
             for t in time_slots:
-                prob += lp.lpSum(x_c[(n, t)] for n in clerks) + cmr[t] >= mini_clerks[t]
+                prob += lp.lpSum(x_c[(n, t)] for n in clerks) + cons_relax[Clerk_m_r][t] >= mini_clerks[t]
     # constraint for standard work time and overtime
     if data[Clerk_m_t][0]:
         clerks_mt = data[Clerk_m_t][3][Clerk_i_d]
@@ -133,10 +138,10 @@ def solve_single_floor(data):
             for n in clerks_mt:
                 prob += lp.lpSum(x_c[(n, t)] for t in time_slots) >= mt
         else:
-            cmt = lp.LpVariable.dicts('Number of less time', clerks_mt, 0)
-            obj += lp.lpSum(cmt[n] for n in clerks_mt) * (10**data[Clerk_m_t][2])
+            cons_relax[Clerk_m_t] = lp.LpVariable.dicts('Number of less time', clerks_mt, 0)
+            obj += lp.lpSum(cons_relax[Clerk_m_t][n] for n in clerks_mt) * (10 ** data[Clerk_m_t][2])
             for n in clerks_mt:
-                prob += lp.lpSum(x_c[(n, t)] for t in time_slots) + cmt[n] >= mt
+                prob += lp.lpSum(x_c[(n, t)] for t in time_slots) + cons_relax[Clerk_m_t][n] >= mt
     if data[Clerk_o_t][0]:
         clerk_ot = data[Clerk_o_t][3][Clerk_i_d]
         ot = data[Clerk_o_t][3]['maximum work time']
@@ -144,10 +149,10 @@ def solve_single_floor(data):
             for n in clerk_ot:
                 prob += lp.lpSum(x_c[(n, t)] for t in time_slots) <= ot
         else:
-            cot = lp.LpVariable.dicts('Number of overtime', clerk_ot, 0)
-            obj += lp.lpSum(cot[n] for n in clerk_ot) * (10**data[Clerk_o_t][2])
+            cons_relax[Clerk_o_t] = lp.LpVariable.dicts('Number of overtime', clerk_ot, 0)
+            obj += lp.lpSum(cons_relax[Clerk_o_t][n] for n in clerk_ot) * (10 ** data[Clerk_o_t][2])
             for n in clerk_ot:
-                prob += lp.lpSum(x_c[(n, t)] for t in time_slots) <= ot + cot[n]
+                prob += lp.lpSum(x_c[(n, t)] for t in time_slots) <= ot + cons_relax[Clerk_o_t][n]
     # constraint for most continuous off days
     if data[Clerk_c_o][0]:
         clerk_co = data[Clerk_c_o][3][Clerk_i_d]
@@ -158,10 +163,10 @@ def solve_single_floor(data):
             for n, d in cod_clerk_days:
                 prob += lp.lpSum(z_c[(n, d+i-1)] for i in range(1, cod+2)) >= 1
         else:
-            codd = lp.LpVariable.dicts('Number of cod', cod_clerk_days, 0)
-            obj += lp.lpSum(codd[n_d] for n_d in cod_clerk_days) * (10**data[Clerk_c_o][2])
+            cons_relax[Clerk_c_o] = lp.LpVariable.dicts('Number of cod', cod_clerk_days, 0)
+            obj += lp.lpSum(cons_relax[Clerk_c_o][n_d] for n_d in cod_clerk_days) * (10 ** data[Clerk_c_o][2])
             for n, d in cod_clerk_days:
-                prob += lp.lpSum(z_c[(n, d + i - 1)] for i in range(1, cod + 2)) + codd[(n, d)] >= 1
+                prob += lp.lpSum(z_c[(n, d + i - 1)] for i in range(1, cod + 2)) + cons_relax[Clerk_c_o][(n, d)] >= 1
     # constraint for work day balance in different weeks
     if data[Clerk_w_b][0]:
         clerk_wb = data[Clerk_w_b][3][Clerk_i_d]
@@ -173,11 +178,11 @@ def solve_single_floor(data):
                 prob += (lp.lpSum(z_c[(n, d1+i)] for i in range(7)) -
                          lp.lpSum(z_c[(n, d2+i)] for i in range(7)) <= wb_dif_b)
         else:
-            cwb = lp.LpVariable.dicts('Number of work day balance', wb_clerk_d, 0)
-            obj += lp.lpSum(cwb[ndd] for ndd in wb_clerk_d) * (10**data[Clerk_w_b][2])
+            cons_relax[Clerk_w_b] = lp.LpVariable.dicts('Number of work day balance', wb_clerk_d, 0)
+            obj += lp.lpSum(cons_relax[Clerk_w_b][ndd] for ndd in wb_clerk_d) * (10 ** data[Clerk_w_b][2])
             for n, d1, d2 in wb_clerk_d:
                 prob += (lp.lpSum(z_c[(n, d1 + i)] for i in range(7)) -
-                         lp.lpSum(z_c[(n, d2 + i)] for i in range(7)) <= wb_dif_b + cwb[(n, d1, d2)])
+                         lp.lpSum(z_c[(n, d2 + i)] for i in range(7)) <= wb_dif_b + cons_relax[Clerk_w_b][(n, d1, d2)])
     # early late switch
     if data[Clerk_e_l_s][0]:
         clerk_els = data[Clerk_e_l_s][3][Clerk_i_d]
@@ -191,15 +196,79 @@ def solve_single_floor(data):
                     else:
                         prob += x_c[(n, hour_num * d)] + x_c[(n, hour_num * d + 1)] <= 1
         else:
-            cels = lp.LpVariable.dicts('Number of unfavorable switch', els_clerk_day, 0)
-            obj += lp.lpSum(cels[n_d] for n_d in els_clerk_day) * (10**data[Clerk_e_l_s][2])
+            cons_relax[Clerk_e_l_s] = lp.LpVariable.dicts('Number of unfavorable switch', els_clerk_day, 0)
+            obj += lp.lpSum(cons_relax[Clerk_e_l_s][n_d] for n_d in els_clerk_day) * (10 ** data[Clerk_e_l_s][2])
             for n in clerk_els:
                 for d in range(1, day_num):
                     if d in els_exception:
                         prob += x_c[(n, hour_num * d)] + x_c[(n, hour_num * d + 1)] <= 2
                     else:
-                        prob += x_c[(n, hour_num * d)] + x_c[(n, hour_num * d + 1)] <= 1 + cels[(n, d)]
+                        prob += (x_c[(n, hour_num * d)] + x_c[(n, hour_num * d + 1)] <=
+                                 1 + cons_relax[Clerk_e_l_s][(n, d)])
     # add object
     prob += obj
-    prob.solve()
-    return None
+    prob.solve(lp.PULP_CBC_CMD(msg=1, maxSeconds=1200))
+    if lp.LpStatus[prob.status] != 'Infeasible':
+        obj_res = lp.value(prob.objective)
+        return obj_res, cons_relax, x_c, z_c, kesi_c, lp.LpStatus[prob.status]
+    else:
+        return None, cons_relax, None, None, None, lp.LpStatus[prob.status]
+
+
+def test():
+    data = dict()
+    data['working hours'], data['working days'], data['clerks'] = 12, 31, 4
+    clerks = range(1, 5)
+    data[Clerk_m_s_h] = (True, False, 1, {'minimum hour': 6})
+    data[Clerk_m_s] = (True, False, 1, None)
+    data[Clerk_w_t_b] = (True, False, 1, {Clerk_i_d: clerks, 'Difference bound': 3})
+    data[Clerk_w_d_b] = (True, False, 1, {Clerk_i_d: clerks, 'Difference bound': 2})
+    data[Clerk_m_o_d] = (True, False, 1, {Clerk_i_d: clerks, 'Min off day': 31*2.0/7})
+    data[Clerk_m_w_d] = (True, False, 1, {Clerk_i_d: clerks, 'Most whole day': 3})
+    data[Clerk_m_r] = (True, False, 1, {'minimum clerk num': [None, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2,
+                                                              2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1,
+                                                              1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+                                                              2, 2,
+                                                              2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2,
+                                                              2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+                                                              1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2,
+                                                              2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2,
+                                                              2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3,
+                                                              3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2,
+                                                              2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,
+                                                              1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2,
+                                                              2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                                                              2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2,
+                                                              2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1,
+                                                              2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+                                                              2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2,
+                                                              2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                                                              2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2,
+                                                              2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
+                                                              1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1]})
+    data[Clerk_m_t] = (True, False, 1, {Clerk_i_d: clerks, 'minimum work time': 167})
+    data[Clerk_o_t] = (True, False, 0, {Clerk_i_d: clerks, 'maximum work time': 167+0})
+    data[Clerk_c_o] = (True, False, 1, {Clerk_i_d: clerks, 'maximum off days': 3})
+    data[Clerk_w_b] = (True, False, 1, {Clerk_i_d: clerks, 'Weekly work day differences': 1, 'Week start days': [1, 8,
+                                                                                                                 15,
+                                                                                                                 22]})
+    tttjust = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    p_days = set()
+    for d in range(1, 32):
+        for h in range(1, 13):
+            t = (d-1) * 12 + h
+            if tttjust[t] > 0:
+                p_days.add(d)
+    data[Clerk_e_l_s] = (True, False, 0, {Clerk_i_d: clerks, 'Switch permission': p_days})
+    return solve_single_floor(data)
